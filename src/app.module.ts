77e75a5e7b2e7b31892f7c08dca5977e51802145
alekseng <alexvdche@mail.ru>
2026-04-04 01:45:25 +0300

    feat: add global request validation and Swagger documentation setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,1 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+import { Module } from '@nestjs/common';import { ConfigModule } from '@nestjs/config';import { AppController } from './app.controller';import { AppService } from './app.service';@Module({  imports: [ConfigModule.forRoot({ isGlobal: true })],  controllers: [AppController],  providers: [AppService],})export class AppModule {}
