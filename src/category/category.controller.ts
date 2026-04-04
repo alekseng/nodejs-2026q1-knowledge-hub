be3b37f9b35d1aaabdb3339d904fba6671a93950
alekseng <alexvdche@mail.ru>
2026-04-04 13:15:23 +0300

    feat: add CRUD endpoints for Category with validation and DTOs

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,9 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CategoryService } from './category.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @ApiTags('category')
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
+
+  @Get()
+  findAll() {
+    return this.categoryService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.categoryService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createCategoryDto: CreateCategoryDto) {
+    return this.categoryService.create(createCategoryDto);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ) {
+    return this.categoryService.update(id, updateCategoryDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    this.categoryService.remove(id);
+  }
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,7 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 import { Category } from './category.interface';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Injectable()
 export class CategoryService {
   private categories: Category[] = [];
+
+  findAll() {
+    return this.categories;
+  }
+
+  findOne(id: string) {
+    const category = this.categories.find((c) => c.id === id);
+    if (!category) {
+      throw new NotFoundException(`Category with id ${id} not found`);
+    }
+    return category;
+  }
+
+  create(createCategoryDto: CreateCategoryDto) {
+    const newCategory: Category = {
+      id: randomUUID(),
+      ...createCategoryDto,
+    };
+    this.categories.push(newCategory);
+    return newCategory;
+  }
+
+  update(id: string, updateCategoryDto: UpdateCategoryDto) {
+    const category = this.findOne(id);
+    if (updateCategoryDto.name) category.name = updateCategoryDto.name;
+    if (updateCategoryDto.description)
+      category.description = updateCategoryDto.description;
+    return category;
+  }
+
+  remove(id: string) {
+    const index = this.categories.findIndex((c) => c.id === id);
+    if (index === -1) {
+      throw new NotFoundException(`Category with id ${id} not found`);
+    }
+    this.categories.splice(index, 1);
+  }
 }
