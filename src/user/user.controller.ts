3f20282033f708bf550f6bfb98073cc9bc62a795
alekseng <alexvdche@mail.ru>
2026-04-04 03:24:37 +0300

    feat: add endpoints to fetch all users and find user by ID

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 
@@ -6,4 +6,14 @@ import { UserService } from './user.service';
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.userService.findOne(id);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { User } from './user.interface';
 
 @Injectable()
 export class UserService {
   private users: User[] = [];
+
+  findAll() {
+    return this.users.map((user) => this.toResponse(user));
+  }
+
+  findOne(id: string) {
+    const user = this.users.find((u) => u.id === id);
+    if (!user) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+    return this.toResponse(user);
+  }
+
+  private toResponse(user: User) {
+    const { password, ...result } = user;
+    return result;
+  }
 }
